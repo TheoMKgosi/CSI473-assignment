f31a7becba645f337fc0a6ef6380b821b851a49d
TheoMKgosi <theo.kgosiemang12@gmail.com>
2025-10-28 09:52:31 +0200

    Fixes for security app

--- assignment/security/app/App.js
+++ assignment/security/app/App.js
@@ -3,11 +3,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import LoginScreen from './components/LoginScreen';
-import SignUpScreen from './components/SignUpScreen';
-import HomeScreen from './components/HomeScreen';
-import ScanScreen from './components/ScanScreen';
-import SettingsScreen from './components/SettingsScreen';
+import LoginScreen from '../components/LoginScreen';
+import SignUpScreen from '../components/SignUpScreen';
+import HomeScreen from '../components/HomeScreen';
+import ScanScreen from '../components/ScanScreen';
+import SettingsScreen from '../components/SettingsScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
